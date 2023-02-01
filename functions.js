fc78948d5bef598396f5a96c7729df51c1e5c10d
mirmik <mirmikns@yandex.ru>
2023-02-01 17:51:17 +0300

    'a'

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 
 function UpdaterFunction(arg) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost/updater/update/" + arg + "/helloworld?async=1", true);
+    xhr.open("GET", "updater/update/" + arg + "/helloworld?async=1", true);
     xhr.send();
 }
